6642fce2aaf4d12cf25b4feadd02ef6815d88667
cyon3 <cyon3@LAPTOP-NAD0LFUO>
2020-05-25 03:21:56 +0900

    20200525_paymentInfo point_sangwon

--- funthing/src/payment/controller/PaymentServlet.java
+++ funthing/src/payment/controller/PaymentServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import member.model.vo.MemberPoint;
 import payment.model.service.PaymentService;
 import payment.model.vo.Payment;
 
@@ -41,17 +42,25 @@ public class PaymentServlet extends HttpServlet {
 			String rcv_adrs = request.getParameter("rcv_adrs");
 			String rcv_phone = request.getParameter("rcv_phone");
 			String comment = request.getParameter("comment");
-			int point_use = Integer.valueOf(request.getParameter("point_use"));
+			int point_use = Integer.valueOf(request.getParameter("point_use"));		//사용한 적립금
 			int total_price = Integer.valueOf(request.getParameter("total_price"));
 			String ship_price = request.getParameter("ship_price");
 //			String expt_price = request.getParameter("expt_price");
-			int expt_point = Integer.valueOf(request.getParameter("expt_point"));
+			int expt_point = Integer.valueOf(request.getParameter("expt_point"));	//총 적립예정 적립금
+			int m_point = Integer.valueOf(request.getParameter("m_point"));			//보유 적립금
 			String mNo = request.getParameter("mNo");
 			
+			int resultM_point = m_point + expt_point - point_use;					//member테이블에 update할 m_point
+			
+			int point_amount = expt_point;	//point 테이블 update 적립금액
+			int my_point = resultM_point;	//point 테이블에 update
 			//최종 결제 금액
 			//상품합계 금액 - 적립금 사용
 			int pmnt_price = total_price - point_use;
 			Payment p = new Payment(rcv_name, rcv_adrs, rcv_phone, comment, total_price, point_use, ship_price, pmnt_price, expt_point, mNo);
+			
+			MemberPoint mp = new MemberPoint();
+			
 			
 			int result = pService.insertPayment(p);
 			System.out.println(mNo);
